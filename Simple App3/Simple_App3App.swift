a6dbcfb2598b16246438ea1ae0add271989b6c07
wrhniedens <wrhniedens@gmail.com>
2023-01-15 10:20:48 -0600

    Initial Commit

---
+++ Simple App3/Simple_App3App.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_App3App.swift
+//  Simple App3
+//
+//  Created by Will Niedens on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_App3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
